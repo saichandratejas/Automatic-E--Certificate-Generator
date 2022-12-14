b0edfb264ea3e6e0759b9c10eb048cd888bc70fb
saii tejass_n <saitejassharma@gmail.com>
2022-12-15 01:09:48 +0530

    Create main.py
    Main python source code

---
+++ main.py
@@ -0,0 +1,9 @@
+import cv2
+list_names =['Tejas']
+
+
+for index, name in enumerate(list_names):
+	template =cv2.imread('C:\\Users\\user\\OneDrive\\Desktop\\Certificate Template neww.jpg')
+	cv2.putText(template,name,(817,709),cv2.FONT_HERSHEY_COMPLEX,4,(0,0,255),1)
+	cv2.imwrite(r'C:\Users\user\OneDrive\Desktop\Generated-Certificates\ {name}.jpg',template)
+	print('Processing Certificate {}/{}'.format(index+1,len(list_names)))
