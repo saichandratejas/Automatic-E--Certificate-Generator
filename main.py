783bec254e31257dea0ded0ec7bf16b592c15482
saii tejass_n <saitejassharma@gmail.com>
2022-12-26 20:51:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ list_names =['Tejas','Arjun','Abhinav','Jack','Anirudh','Konda']
 
 for index, name in enumerate(list_names):
 	template =cv2.imread('C:\\Users\\user\\OneDrive\\Desktop\\Certificate Template neww.jpg')
-	cv2.putText(template,name,(817,709),cv2.FONT_HERSHEY_COMPLEX,4,(0,0,255),1)
+	cv2.putText(template,name,(817,709),cv2.FONT_HERSHEY_COMPLEX,4,(0,0,0),4)
 	cv2.imwrite(r'C:\Users\user\OneDrive\Desktop\Generated-Certificates\ {}.jpg'.format(name.strip()),template)
 	print('Processing Certificate {}/{}'.format(index+1,len(list_names)))
